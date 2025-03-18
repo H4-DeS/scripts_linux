47f6af9401c5da47790f8fbcf104c89429b1a082
H4-DeS <oliveira.gonzales@gmail.com>
2025-03-18 01:04:18 +0000

    Arquivo atualizado v.1.1

--- IaC_Apache.sh
+++ IaC_Apache.sh
@@ -11,12 +11,15 @@ echo "Instalando Apache2"
 apt install apache2 -y
 rm /var/www/html/index.html
 
+echo "Descompactando e transferindo conteúdo para servidor web"
+
 apt install unzip
 
 wget -P /tmp http://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip /tmp/main.zip -d /tmp
 
+unzip /tmp/main.zip -d /tmp
 sudo cp -r /tmp/linux-site-dio-main/* /var/www/html
 rm /tmp/linux-site-dio-main
+rm /tmp/main.zip
 
-
+set +x
